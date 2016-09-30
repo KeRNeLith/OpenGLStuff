3b8e9226fb55614613c22d7b6f8fbe65866b36ed
Alexandre Rabérin <kernelith@live.fr>
2016-09-30 22:32:22 +0200

    Add camera interactions (cf. first part of the tp)

--- src/GUI/eventsHandling.cpp
+++ src/GUI/eventsHandling.cpp
@@ -91,6 +91,9 @@ bool WrapperSDL::EventController::handleSDLEvent(SDL_Event* event, SDL_Window* w
 
         if (MouseData::rightButtonPressed)
         {
+            displayParams->camera().updateTargetX(MouseData::mouseX - event->motion.x);
+            displayParams->camera().updateTargetZ(MouseData::mouseY - event->motion.y);
+
             // Mise à jour du modèle
             // Non implémenté
             MouseData::mouseX = event->motion.x; // Enregistrement des nouvelles
@@ -114,6 +117,128 @@ bool WrapperSDL::EventController::handleSDLEvent(SDL_Event* event, SDL_Window* w
 		SDL_GL_SwapWindow(window); // On envoie le buffer à l'écran 
 	}
 		break;
+
+    //////////////////////////////////////////////////////
+    // Événements liés au clavier
+    case SDL_KEYDOWN:
+    {
+        int direction = -1;
+        switch (event->key.keysym.sym)
+        {
+        case SDLK_x:
+            if (event->key.keysym.mod & KMOD_SHIFT)
+            {
+                // Handle 'X'
+                direction *= -1;
+            }
+            /*else
+            {
+                // Handle 'x'
+            }*/
+
+            displayParams->camera().updatePosX(GLdouble(direction));
+            break;
+
+        case SDLK_y:
+            if (event->key.keysym.mod & KMOD_SHIFT)
+            {
+                // Handle 'Y'
+                direction *= -1;
+            }
+            /*else
+            {
+                // Handle 'y'
+            }*/
+
+            displayParams->camera().updatePosY(GLdouble(direction));
+            break;
+
+        case SDLK_z:
+            if (event->key.keysym.mod & KMOD_SHIFT)
+            {
+                // Handle 'Z'
+                direction *= -1;
+            }
+            /*else
+            {
+                // Handle 'z'
+            }*/
+
+            displayParams->camera().updatePosZ(GLdouble(direction));
+            break;
+
+        case SDLK_a:
+            if (event->key.keysym.mod & KMOD_SHIFT)
+            {
+                // Handle 'A'
+                direction *= -1;
+            }
+            /*else
+            {
+                // Handle 'a'
+            }*/
+
+            displayParams->camera().updateOpenAngle(GLdouble(direction));
+            displayParams->camera().applyPerspectiveProjection();
+            break;
+
+        case SDLK_e:
+        {
+            const GLdouble* center = displayParams->camera().getTargetPoint();
+            const GLdouble* camPos = displayParams->camera().getPosition();
+
+            GLdouble newPos[3] =
+            {
+                center[0] + 1.1 * (camPos[0] - center[0]),
+                center[1] + 1.1 * (camPos[1] - center[1]),
+                center[2] + 1.1 * (camPos[2] - center[2])
+            };
+
+            displayParams->camera().setPosition(newPos);
+
+            break;
+        }
+
+        case SDLK_r:
+        {
+            const GLdouble* center = displayParams->camera().getTargetPoint();
+            const GLdouble* camPos = displayParams->camera().getPosition();
+
+            GLdouble newPos[3] =
+            {
+                center[0] + 0.9 * (camPos[0] - center[0]),
+                center[1] + 0.9 * (camPos[1] - center[1]),
+                center[2] + 0.9 * (camPos[2] - center[2])
+            };
+
+            displayParams->camera().setPosition(newPos);
+
+            break;
+        }
+
+        case SDLK_UP:
+            displayParams->camera().updatePosY(GLdouble(1));
+            break;
+
+        case SDLK_DOWN:
+            displayParams->camera().updatePosY(GLdouble(-1));
+            break;
+
+        case SDLK_LEFT:
+            displayParams->camera().updateTargetY(-1);
+            break;
+
+        case SDLK_RIGHT:
+            displayParams->camera().updateTargetY(1);
+            break;
+
+        default:
+            std::cerr << "Touche non gérée" << std::endl;
+            break;
+        }
+
+        break;
+    }
 
 	//////////////////////////////////////////////////////
 	// Événements utilisateur sur la fenêtre graphique
